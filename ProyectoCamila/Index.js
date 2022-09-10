797ba72d7beff9bebd564ee52ef34beeac85e3f9
camiarru <43612564+camiarru@users.noreply.github.com>
2022-09-10 20:34:20 -0300

    Update Index.js

--- ProyectoCamila/Index.js
+++ ProyectoCamila/Index.js
@@ -1,54 +1,77 @@
-
 // Web de juegos
 // Funciones de posible uso en el proyecto final
 
+let id;
 let nombre;
 let apodo;
 let mail;
 let edad;
-let puntosJugador;
+let puntos;
+const arrayJugadores = [];
 
-//Registro de datos de jugador
-function registroJugador() {
-nombre = prompt("Nombre:");
-apodo = prompt ("Como quisiera ser llamadx?");
-mail = prompt("Mail");
-edad = parseInt(prompt ("Edad"));
-alert (`Bienvenidx ${apodo}! Su registro se ha completado con exito!`);
-}
 
-//cargar puntos ganados
-function sumarPuntos(puntajeJugador, puntosGanados){
-   puntajeJugador = parseInt(puntajeJugador); //En un futuro la validacion viene ya de los puntos enviados
-   puntosGanados = parseInt (puntosGanados);
+function sumarPuntos(puntajeJugador, puntosGanados) {
    let nuevoPuntaje = puntajeJugador + puntosGanados;
    return nuevoPuntaje;
 }
 
-//Validacion e inicio 
+// constructor de Jugadores
+class Jugador{
+   constructor (id, nombre, apodo, mail, edad, puntos){
+       this.id = id;
+       this.nombre = nombre;
+       this.apodo = apodo;
+       this.mail = mail;
+       this.edad = edad;
+       this.puntos = puntos;
+   }
+}
+
+//Registro de datos de jugador
+function registroJugador() {
+   let numArray;
+   nombre = prompt("Nombre:");
+   apodo = prompt ("Como quisiera ser llamadx?");
+   mail = prompt("Mail");
+   edad = parseInt(prompt ("Edad"));
+   alert (`Bienvenidx ${apodo}! Su registro se ha completado con exito!`);
+   
+   console.log(nombre);
+   console.log(apodo);
+   console.log(edad);
+   console.log(mail);
+}
+
+//Validacion e inicio
 function inicio (){
 
    cantJugadores = parseInt(prompt ("Bienvenidx! Cuantxs jugadorxs participaran del torneo?"));
    if (cantJugadores > 0){
-         for (let jugadores = 0; jugadores < cantJugadores; jugadores++) {
-         alert (`Jugador ${jugadores+1}, ingrese sus datos`);
+         for (let i = 0; i < cantJugadores; i++) {
+         alert (`Jugador ${i+1}, ingrese sus datos`);
          registroJugador();
-         } 
-         alert (`Se ingresaron lxs ${cantJugadores} jugadorxs! QUE COMIENCE EL JUEGO!`); 
+       
+         //Indicamos que cada jugador cargado es un elemento en un array
+         arrayJugadores.push(new Jugador (i, nombre, apodo, mail, edad, 0));
+         console.log(arrayJugadores[i]);
 
+         for (const jugador of arrayJugadores){
+            if (jugador.id == i) {
+               let puntajeJugador = parseInt(arrayJugadores[i].puntos);
+               let puntosGanados = parseInt(prompt ("Ingese el puntaje obtenido"));
+               arrayJugadores[i].puntos = sumarPuntos(puntajeJugador, puntosGanados);
+               alert(`Su nuevo puntaje es ${arrayJugadores[i].puntos}`);
+               console.log(arrayJugadores[i].puntos);
+               //console.log (`este es el for const; ${jugador} =  en el casillero del array: ${i}`);
+              // console.log(`El nombre del jugador ${i} es ${arrayJugadores[i].nombre}`);
+            }
+         }//fin for recorriendo array
+      }//fin for de validacion
    } else{
       alert ("Debe ingresar al menos un jugador!");
       inicio();
    }
 }
 
+
 inicio();
-
-// Cuando finaliza una partida, cada jugador suma sus puntos (se va hacer de manera automatica en un futuro)
-let puntosGanados = prompt("Cuantos puntos sumo en la partida?");
-//imaginando que ya tenia 30 puntos ganados previamente
-puntosJugador = sumarPuntos (30, puntosGanados);
-parseInt(puntosJugador);
-alert (`Su puntaje actual es: ${puntosJugador}`);
-   
-
